90110f80756ce94410239044e9aea3c784efc67f
joshuahaddad <joshuahaddad@ufl.edu>
2022-02-09 14:08:56 -0500

    Removing test code

--- src/omlt/io/sklearn-reader.py
+++ src/omlt/io/sklearn-reader.py
@@ -54,23 +54,3 @@ def load_sklearn_MLP(model, scaling_object=None, input_bounds=None, initial_type
     onx_model = onx.SerializeToString()
 
     return load_onnx_neural_network(onx_model, scaling_object, input_bounds)
-
-
-
-
-
-"""X, y = make_regression(n_samples=200, n_features=42, random_state=1)
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
-regr = MLPRegressor(random_state=1, max_iter=500).fit(X_train, y_train)
-print(regr)
-
-initial_type = [('float_input', FloatTensorType([None, regr.n_features_in_]))]
-onx = convert_sklearn(regr, initial_types=initial_type)
-onx_model = onx.SerializeToString()
-
-sess = rt.InferenceSession(onx_model)
-input_name = sess.get_inputs()[0].name
-label_name = sess.get_outputs()[0].name
-pred_onx = sess.run([label_name], {input_name: X_test.astype(numpy.float32)})[0].transpose()[0]
-pred_sklearn = regr.predict(X_test.astype(numpy.float32))
-print(max(abs(pred_onx - pred_sklearn)/pred_sklearn*100))"""
