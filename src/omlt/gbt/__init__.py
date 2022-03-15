3e2f9e41b09f14db04b5633889e9be72d9796f42
at1618 <alexander.thebelt18@imperial.ac.uk>
2022-03-15 23:00:15 +0000

    add definition

--- src/omlt/gbt/__init__.py
+++ src/omlt/gbt/__init__.py
@@ -4,10 +4,11 @@ We use the following notation to describe the gradient-boosted trees formulation
 .. math::
 
     \begin{align*}
+    \hat{\mu} &:= \text{Mean prediction of tree ensemble}\\
     T &:= \text{Set of trees in ensemble}\\
     L_t &:= \text{Set of leaves in tree $t$}\\
-    z_{t,l} &:= \text{Binary variable indicating if leaf $l$ in
-        tree $t$ is active}\\
+    z_{t,l} &:= \text{Binary variable indicating if leaf $l$
+        in tree $t$ is active}\\
     \text{Left}_{t,s} &:= \text{Set of leaf variables left of split $s$
         in tree $t$}\\
     \text{Right}_{t,s} &:= \text{Set of leaf variables right of split $s$
@@ -17,8 +18,8 @@ We use the following notation to describe the gradient-boosted trees formulation
     j(s) &:= \text{index of split $s$}\\
     V_t &:= \text{Set of splits in tree $t$}\\
     n &:= \text{Index set of input features}\\
-    m_i &= \text{Index set of splits for feature $i$}\\
-    F_{t,l} &= \text{Weight of leaf $l$ in tree $t$}\\
+    m_i &:= \text{Index set of splits for feature $i$}\\
+    F_{t,l} &:= \text{Weight of leaf $l$ in tree $t$}\\
     \end{align*}
 """
 from omlt.gbt.gbt_formulation import GBTBigMFormulation
