76c790cc8ba7bf9d10bae1280d8a938cdf22c2d3
Jordan Jalving <jhjalving@gmail.com>
2022-07-05 21:36:45 -0600

    fix test

--- tests/neuralnet/test_network_definition.py
+++ tests/neuralnet/test_network_definition.py
@@ -3,6 +3,7 @@ import pyomo.environ as pyo
 import pytest
 
 from omlt.block import OmltBlock
+from omlt.neuralnet.layer import DenseLayer, InputLayer
 from omlt.neuralnet.network_definition import NetworkDefinition
 from omlt.neuralnet.nn_formulation import FullSpaceNNFormulation
 from omlt.scaling import OffsetScaling
