16a985a3b3430af5fc0534f46da976928faf4e08
Jordan Jalving <jhjalving@gmail.com>
2022-10-25 12:03:52 -0700

    update formatting

--- src/omlt/io/__init__.py
+++ src/omlt/io/__init__.py
@@ -1,6 +1,6 @@
 from pyomo.common.dependencies import attempt_import
 
-# check for onnx
+# check for onnx and create shortcut if available
 onnx, onnx_available = attempt_import("onnx")
 if onnx_available:
     from omlt.io.onnx import (
@@ -9,7 +9,7 @@ if onnx_available:
         write_onnx_model_with_bounds,
     )
 
-# check for keras
+# check for keras and create shortcut if available
 keras, keras_available = attempt_import("tensorflow.keras")
 if keras_available:
     from omlt.io.keras import load_keras_sequential
