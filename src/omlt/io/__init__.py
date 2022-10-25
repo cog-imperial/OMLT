c1d2a66bf24a517650ae897816d65d0b43046501
Jordan Jalving <jhjalving@gmail.com>
2022-10-25 11:27:55 -0700

    update io __init__.py with attempt import

--- src/omlt/io/__init__.py
+++ src/omlt/io/__init__.py
@@ -1,5 +1,15 @@
-from omlt.io.onnx import (
-    load_onnx_neural_network,
-    load_onnx_neural_network_with_bounds,
-    write_onnx_model_with_bounds,
-)
+from pyomo.common.dependencies import attempt_import
+
+# check for onnx
+onnx, onnx_available = attempt_import("onnx")
+if onnx_available:
+    from omlt.io.onnx import (
+        load_onnx_neural_network,
+        load_onnx_neural_network_with_bounds,
+        write_onnx_model_with_bounds,
+    )
+
+# check for keras
+keras, keras_available = attempt_import("tensorflow.keras")
+if keras_available:
+    from omlt.io.keras import load_keras_sequential
