6dd902ed8300701b6f5fabbab501da34c88f8f25
Carl Laird <carldlaird@users.noreply.github.com>
2022-11-26 07:30:49 -0600

    working on code coverage

--- src/omlt/dependencies.py
+++ src/omlt/dependencies.py
@@ -2,12 +2,6 @@ from pyomo.common.dependencies import attempt_import
 
 # check for onnx and create shortcut if available
 onnx, onnx_available = attempt_import("onnx")
-if onnx_available:
-    from omlt.io.onnx import (
-        load_onnx_neural_network,
-        load_onnx_neural_network_with_bounds,
-        write_onnx_model_with_bounds,
-    )
 
 keras, keras_available = attempt_import("tensorflow.keras")
 
