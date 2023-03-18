54fe575fcce6119ac8e63857df12819a55106e15
Shumeng Lin <54089164+linshumeng@users.noreply.github.com>
2023-03-18 19:37:54 -0400

    Upload script for testing LinearTreeModel Test dictionary

---
+++ tests/lineartree/test_lt_formulation.py
@@ -0,0 +1,90 @@
+import numpy as np
+import pyomo.environ as pe
+from lineartree import LinearTreeRegressor
+from sklearn.linear_model import LinearRegression
+
+from omlt import OmltBlock
+from omlt.lineartree import LinearTreeGDPFormulation, LinearTreeModel
+import omlt
+
+X_small = np.array([[-0.68984135],
+    [ 0.91672866],
+    [-1.05874972],
+    [ 0.95275351],
+    [ 1.03796615],
+    [ 0.45117668],
+    [-0.14704376],
+    [ 1.66043409],
+    [-0.73972191],
+    [-0.8176603 ],
+    [ 0.96175973],
+    [-1.238874  ],
+    [-0.97492265],
+    [ 1.07121986],
+    [-0.95379269],
+    [-0.86546252],
+    [ 0.8277057 ],
+    [ 0.50486757],
+    [-1.38435899],
+    [ 1.54092856]])
+y_small = np.array([[ 0.04296633],
+    [-0.78349216],
+    [ 0.27114188],
+    [-0.58516476],
+    [-0.15997756],
+    [-0.37529212],
+    [-1.49249696],
+    [ 1.56412122],
+    [ 0.18697725],
+    [ 0.4035928 ],
+    [-0.53231771],
+    [-0.02669967],
+    [ 0.36972983],
+    [ 0.09201347],
+    [ 0.44041505],
+    [ 0.46047019],
+    [-1.04855941],
+    [-0.586915  ],
+    [ 0.15472157],
+    [ 1.71225268]])
+
+def linear_model_tree(X, y):
+    regr = LinearTreeRegressor(LinearRegression(), criterion='mse', max_depth=5)
+    regr.fit(X, y)
+    return regr
+
+def test_linear_tree_model():
+    regr_small = linear_model_tree(X=X_small, y=y_small)
+    # check the type of the lmt
+    assert(str(type(regr_small)) == "<class 'lineartree.lineartree.LinearTreeRegressor'>")
+    ltmodel_small = LinearTreeModel(regr_small)
+    # TODO: is _scaling_object, _scaled_input_bounds are essential?
+    assert(ltmodel_small._n_inputs == 1)
+    assert(ltmodel_small._n_outputs == 1)
+    # test for splits
+    assert(ltmodel_small._splits.keys() == 5)
+    splits_key_list = ['col', 'th', 'loss', 'samples', 'parent', 'children', 'models', 'left_leaves', 'right_leaves', 'y_index']
+    for i in ltmodel_small._splits.keys():
+        for key in ltmodel_small[i]._splits.keys():
+            assert(key in splits_key_list)
+    # test for leaves
+    assert(ltmodel_small._leaves.keys() == 6)
+    leaves_key_list = ['loss', 'samples', 'models', 'slope', 'intercept', 'parent', 'bounds']
+    for j in ltmodel_small._leaves.keys():
+        for key in ltmodel_small[j]._leaves.keys():
+            assert(key in leaves_key_list)
+    # TODO: do we need to test slope and bounds in leaves_dic
+    # test for thresholds
+    # test whether each feature has threshold
+    assert(len(ltmodel_small._thresholds.keys()) == ltmodel_small._n_inputs)
+    thresholds_count = 0
+    for _ in range(len(ltmodel_small._threholds.keys())):
+        thresholds_count += 1
+    # test number of thresholds
+    assert(thresholds_count == ltmodel_small._splits.keys())
+
+    pass
+
+# TODO: not sure whether we would like to test different transformations
+def test_formulation():
+    pass
