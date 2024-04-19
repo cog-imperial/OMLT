b57848ae4f981cd1b69d3c9c5df2b9b1f7f63119
Jeremy Sadler <53983960+jezsadler@users.noreply.github.com>
2024-04-19 16:58:01 -0700

    Getting dependencies lined up correctly

--- src/omlt/base/__init__.py
+++ src/omlt/base/__init__.py
@@ -1,6 +1,8 @@
 DEFAULT_MODELING_LANGUAGE = "pyomo"
+from omlt.dependencies import julia_available
 
-from omlt.base.julia import jump
+if julia_available:
+  from omlt.base.julia import jump
 from omlt.base.var import OmltVar
 
 # from omlt.base.expression import OmltExpression
