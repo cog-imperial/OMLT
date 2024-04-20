cad7ba28236f1cec5a13ba87f3df75a068023ee6
Jeremy Sadler <53983960+jezsadler@users.noreply.github.com>
2024-04-19 17:35:36 -0700

    Update var.py

--- src/omlt/base/var.py
+++ src/omlt/base/var.py
@@ -11,8 +11,9 @@ import pyomo.environ as pyo
 
 from omlt.dependencies import julia_available
 
+from omlt.base import DEFAULT_MODELING_LANGUAGE
 if julia_available:
-    from omlt.base import jump, DEFAULT_MODELING_LANGUAGE
+    from omlt.base import jump
 from omlt.base.julia import JuMPVarInfo, JumpVar
 
 
