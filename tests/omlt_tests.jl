e144f5b6b6005d5418dc55343c9920d2a6e22e56
Jeremy Sadler <53983960+jezsadler@users.noreply.github.com>
2025-02-17 05:51:19 +0000

    switching to relative paths for test models

--- tests/omlt_tests.jl
+++ tests/omlt_tests.jl
@@ -153,7 +153,7 @@ end
     )
     scaled_input_bounds = Dict(0 => (0,5))
     
-    path = "/workspaces/OMLT/tests/models/keras_linear_131_sigmoid.onnx"
+    path = "models/keras_linear_131_sigmoid.onnx"
     
     py_model = onnx_py.load(path)
     net = omlt_io.load_onnx_neural_network(py_model, scaler, scaled_input_bounds)
@@ -190,7 +190,7 @@ end
     )
     scaled_input_bounds = Dict(0 => (0,5))
     
-    path = "/workspaces/OMLT/tests/models/keras_linear_131.onnx"
+    path = "models/keras_linear_131.onnx"
     
     py_model = onnx_py.load(path)
     net = omlt_io.load_onnx_neural_network(py_model, scaler, scaled_input_bounds)
