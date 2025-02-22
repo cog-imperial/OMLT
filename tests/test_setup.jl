fcea422884da578d95c8d5ea824fee56cfdc60fa
Jeremy Sadler <53983960+jezsadler@users.noreply.github.com>
2025-02-22 23:52:46 +0000

    fixing Julia test set up

--- tests/test_setup.jl
+++ tests/test_setup.jl
@@ -2,5 +2,5 @@ using Pkg
 Pkg.add(["Test", "CondaPkg", "PythonCall", "IJulia", "JuMP", "Ipopt", "HiGHS"])
 
 using CondaPkg
-CondaPkg.add("pyomo", version="6.8.0")
+CondaPkg.add("pyomo", version="==6.8.0")
 CondaPkg.add_pip("omlt", version="@file:///home/runner/work/OMLT/OMLT/")
